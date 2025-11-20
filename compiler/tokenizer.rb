cd7251b787d56f13d9fe5708036befbf7e274e40
Paulyn <87922888+JakeOJeff@users.noreply.github.com>
2025-11-21 02:15:41 +0530

    Setting TOKEN types in Lexer

--- compiler/tokenizer.rb
+++ compiler/tokenizer.rb
@@ -1,5 +1,23 @@
 class Tokenizer
 
+  TOKEN_TYPES = [
+    [:def, /\bcall\b/],
+    [:end, /\bpop\b/],
+    [:identifier, /\ba-zA-Z]+\b/],
+    [:integer, /\b[0-9]+\b/],
+    [:oparen, /\(/],
+    [:cparen, /\)/],
+  ]
+  def initialize(code)
+    @code = code
+  end
+
+  def tokenize
+    until @code.empty?
+      TOKEN_TYPES.each do 
+    end
+  end
 end
 
-tokens = Tokenizer.new(File.read(""))
+tokens = Tokenizer.new(File.read("examples/main.lat")).tokenize 
+p tokens
