4f27051dff76f04e4fe5b0174009e481614ba15f
Paulyn <87922888+JakeOJeff@users.noreply.github.com>
2026-05-24 01:11:51 +0530

    Fixing the dir issue of repo root

--- install/install.bat
+++ install/install.bat
@@ -14,10 +14,20 @@ if errorlevel 1 (
 for /f "tokens=*" %%i in ('where ruby') do set RUBY_PATH=%%i 
 echo [lat] Found Ruby at %RUBY_PATH%
 
-set SCRIPT_DIR="%~dp0"
-set COMPILER_DIR=%SCRIPT_DIR%..\compiler 
+set SCRIPT_DIR=%~dp0
+
+pushd "%SCRIPT_DIR%.."
+set REPO_DIR=%CD%
+popd
+
+set COMPILER_DIR=%REPO_DIR%\compiler
 
 if not exist %COMPILER_DIR% (
     echo [lat] ERROR: compiler\ folder not found. Run this from lat repo root.
+    exit /b 1
+)
+
+if not exist "%COMPILER_DIR%\compile.rb" (
+    echo [lat] ERROR: compiler\compile.rb not found.
     exit /b 1
 )
